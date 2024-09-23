7e6fa2e75accc5609082d00c3ac3fda19c69ff0e
Andres Vazquez <andres.vazquez.martin@gmail.com>
2024-09-23 20:52:17 +0200

    Creada función jugar y añadida función de reinicio del juego

--- Adivina.java
+++ Adivina.java
@@ -1,30 +1,49 @@
-
 import java.util.Random;
 import java.util.Scanner;
 
 public class Adivina {
-    public static void main (String[] args){
-        Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
-        int randomNum = random.nextInt(0,100);
-        System.out.println("Adivina el número que estoy pensando: ");
+    
+    
+    public static void jugar(Scanner scanner, Random random) {
+        int randomNum = random.nextInt(100);
+        System.out.println("Adivina el número que estoy pensando (entre 0 y 100): ");
         int numIntroducido = scanner.nextInt();
         int conteo = 0;
 
-        while (numIntroducido != randomNum){
-            if (numIntroducido > randomNum){
-                conteo++;
-                System.out.println("El numero es menor, es tu intento nº " + conteo + ". Prueba con otro número.");
+        while (numIntroducido != randomNum) {
+            conteo++;
+            if (numIntroducido > randomNum) {
+                System.out.println("El número es menor, es tu intento nº " + conteo + ". Prueba con otro número.");
             } else {
-                conteo++;
-                System.out.println("El numero es mayor, es tu intento nº " + conteo + " Prueba con otro número.");
+                System.out.println("El número es mayor, es tu intento nº " + conteo + ". Prueba con otro número.");
             }
             numIntroducido = scanner.nextInt();
         }
+        
         conteo++;
-        System.out.println("Correcto! Lo has conseguido al " + conteo +"º intento.");
-
-        scanner.close();
+        System.out.println("¡Correcto! Lo has acertado en el intento nº " + conteo + ".");
     }
     
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        String jugarOtraVez;
+
+        
+        while (true) {
+            jugar(scanner, random);  
+            
+            
+            System.out.println("¿Quieres jugar de nuevo? (s/n): ");
+            jugarOtraVez = scanner.next();
+            
+            
+            if (!jugarOtraVez.equals("s")) {
+                break;
+            }
+        }
+
+        System.out.println("¡Gracias por jugar!");
+        scanner.close();
+    }
 }
