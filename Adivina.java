2d03d964e4b8ffb55e676a76bee66a146b1640cc
andreamsgi27 <andreamsgi27@hotmail.com>
2024-09-23 12:56:50 +0200

    Esqueleto del juego

---
+++ Adivina.java
@@ -0,0 +1,26 @@
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Adivina {
+    public static void main (String[] args){
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int randomnum = random.nextInt(100);
+        System.out.println("Adivina el número que estoy pensando: ");
+        int numintroducido = scanner.nextInt();
+
+        while (numintroducido != randomnum){
+            if (numintroducido > randomnum){
+                System.out.println("El numero es menor!");
+            } else {
+                System.out.println("El numero es mayor!");
+            }
+            numintroducido = scanner.nextInt();
+        }
+
+        System.out.println("Correcto!");
+
+        scanner.close();
+    }
+}
