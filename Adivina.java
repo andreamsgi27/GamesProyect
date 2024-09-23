61e9c7d4a45f6bac2878ad3f9271634ab1d0317e
Andres Vazquez <andres.vazquez.martin@gmail.com>
2024-09-23 14:27:19 +0200

    Conteo añadido y cambios en la sintaxis de los prints

--- Adivina.java
+++ Adivina.java
@@ -6,22 +6,25 @@ public class Adivina {
     public static void main (String[] args){
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
-        int randomnum = random.nextInt(100);
+        int randomNum = random.nextInt(0,100);
         System.out.println("Adivina el número que estoy pensando: ");
-        int numintroducido = scanner.nextInt();
+        int numIntroducido = scanner.nextInt();
+        int conteo = 0;
 
-        while (numintroducido != randomnum){
-            if (numintroducido > randomnum){
-                System.out.println("El numero es menor!");
+        while (numIntroducido != randomNum){
+            if (numIntroducido > randomNum){
+                conteo++;
+                System.out.println("El numero es menor, es tu intento nº " + conteo + ". Prueba con otro número.");
             } else {
-                System.out.println("El numero es mayor!");
+                conteo++;
+                System.out.println("El numero es mayor, es tu intento nº " + conteo + " Prueba con otro número.");
             }
-            numintroducido = scanner.nextInt();
+            numIntroducido = scanner.nextInt();
         }
-
-        System.out.println("Correcto!");
+        conteo++;
+        System.out.println("Correcto! Lo has conseguido al " + conteo +"º intento.");
 
         scanner.close();
     }
-    //prueba de push
+    
 }
