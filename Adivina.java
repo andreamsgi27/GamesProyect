430c98b304b26b0f346f8d8f1a891e862c3a1e52
andreamsgi27 <andreamsgi27@hotmail.com>
2024-09-24 10:01:31 +0200

    Comentamos el código, código ultimado y completado

--- Adivina.java
+++ Adivina.java
@@ -3,18 +3,19 @@ import java.util.Scanner;
 
 public class Adivina {
     
-    
+    //funcion con la lógica del juego
     public static void jugar(Scanner scanner, Random random) {
-        int randomNum = random.nextInt(100);
+        int randomNum = random.nextInt(100); //sentencia para generar número aleatorio de 0 a 100
         System.out.println("Adivina el número que estoy pensando (entre 0 y 100): ");
         int numIntroducido = scanner.nextInt();
-        int conteo = 0;
+        int conteo = 0; //variable para guardar el numero de intentos hasta acertar
 
+        //bucle while para comparar números
         while (numIntroducido != randomNum) {
-            conteo++;
+            conteo++; //suma 1 al contador de intentos
             if (numIntroducido > randomNum) {
                 System.out.println("El número es menor, es tu intento nº " + conteo + ". Prueba con otro número.");
-            } else {
+            } else { //no haría falta poner condición
                 System.out.println("El número es mayor, es tu intento nº " + conteo + ". Prueba con otro número.");
             }
             numIntroducido = scanner.nextInt();
@@ -29,17 +30,17 @@ public class Adivina {
         Random random = new Random();
         String jugarOtraVez;
 
-        
+        //bucle que ejecuta el juego, hace que se pueda volver a jugar
         while (true) {
-            jugar(scanner, random);  
+            jugar(scanner, random); //se pasan scanner y random como parámetros para que la función funcione con ellos
             
             
-            System.out.println("¿Quieres jugar de nuevo? (s/n): ");
+            System.out.println("¿Quieres jugar de nuevo? (si/no): ");
             jugarOtraVez = scanner.next();
             
             
-            if (!jugarOtraVez.equals("s")) {
-                break;
+            if (!jugarOtraVez.equals("si")) {
+                break; //si no escribes "si", el bucle terminará y pasará a lo último del código
             }
         }
 
