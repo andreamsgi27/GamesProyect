7cc63801728ddec421604f427fc9136934d3d6b0
andresvaz89 <andres.vazquez.martin@gmail.com>
2024-09-26 14:15:20 +0200

    Ahora se muestran las letras que no están en la palabra y se ha aumentado el banco de palabras

--- Ahorcado.java
+++ Ahorcado.java
@@ -2,51 +2,95 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Ahorcado {
-    
-    
-    public static void jugar(Scanner scanner, Random random) {
-        String [] bancoPalabras = {"cambio","espera","sufren","ayudas", "abuela","estepa", "palcos"};
-        int indicePalabraEscogida = random.nextInt(7);
-        String palabraEscogida =bancoPalabras[indicePalabraEscogida];
-        System.out.println(indicePalabraEscogida);
-        System.out.println(palabraEscogida);
-        System.out.println("Introduce una letra: ");
-        String letraIntroducida = scanner.nextLine();
-        System.out.println(letraIntroducida);
 
-        int conteo = 0;
-
-        
+    public static void dibujarAhorcado(int numErrores) {
+        String[] dibujoAhorcado = {
+                "+---+\n    |\n    |\n    |\n   ===",
+                "+---+\n O  |\n    |\n    |\n   ===",
+                "+---+\n O  |\n |  |\n    |\n   ===",
+                "+---+\n O  |\n/|  |\n    |\n   ===",
+                "+---+\n O  |\n/|\\ |\n    |\n   ===",
+                "+---+\n O  |\n/|\\ |\n/   |\n   ===",
+                "+---+\n O  |\n/|\\ |\n/ \\ |\n   ==="
+        };
+        System.out.println(dibujoAhorcado[numErrores]);
     }
-    
-    public static void main (String[] args) {
+
+    public static void jugar(Scanner scanner, Random random) {
+        String[] bancoPalabras = {"cambio", "espera", "sufren", "ayudas", "abuela", 
+            "estepa", "palcos", "elefante", "murcielago", "naranja", 
+            "computadora", "programacion", "java", "desarrollador", 
+            "matematica", "astronomia", "filosofia", "ciencia", 
+            "geografia", "historia", "arte", "musica", "literatura"};
+        int indicePalabraEscogida = random.nextInt(bancoPalabras.length);
+        String palabraEscogida = bancoPalabras[indicePalabraEscogida];
+
+        char[] progreso = new char[palabraEscogida.length()];
+        for (int i = 0; i < progreso.length; i++) {
+            progreso[i] = '_';
+        }
+
+        int numErrores = 0;  
+        int intentosMaximos = 6;  
+        boolean palabraAcertada = false;  
+        String stringsConFallos = ""; 
+
+        while (numErrores < intentosMaximos && !palabraAcertada) {
+            System.out.println("Palabra: " + new String(progreso));
+            System.out.println("Introduce una letra: ");
+            String letraIntroducida = scanner.nextLine();
+
+            if (letraIntroducida.length() != 1 || !Character.isLetter(letraIntroducida.charAt(0))) {
+                System.out.println("Por favor, introduce tan sólo una letra.");
+                continue;
+            }
+
+            char letra = letraIntroducida.charAt(0);
+            boolean acierto = false;
+
+            for (int i = 0; i < palabraEscogida.length(); i++) {
+                if (palabraEscogida.charAt(i) == letra) {
+                    progreso[i] = letra;
+                    acierto = true;
+                }
+            }
+
+            if (acierto) {
+                System.out.println("¡Adivinaste una letra!");
+            } else {
+                if (!stringsConFallos.contains(String.valueOf(letra))) {
+                    stringsConFallos += letra;
+                }
+                numErrores++;
+                System.out.println("La letra " + letra + " es incorrecta.");
+                System.out.println("Ya has usado: " + stringsConFallos);
+            }
+
+            dibujarAhorcado(numErrores);
+            palabraAcertada = new String(progreso).equals(palabraEscogida);
+        }
+
+        if (palabraAcertada) {
+            System.out.println("¡Felicidades! Has adivinado la palabra: " + palabraEscogida);
+        } else {
+            System.out.println("Lo siento, has perdido. La palabra era: " + palabraEscogida);
+        }
+    }
+
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
         String jugarOtraVez;
 
-        
-        
-        while (true) {
-            jugar(scanner, random);  
-            
-            
+        do {
+            jugar(scanner, random);
+
             System.out.println("¿Quieres jugar de nuevo? (s/n): ");
-            jugarOtraVez = scanner.next();
-            
-            
-            if (!jugarOtraVez.equals("s")) {
-                break;
-            }
-        }
+            jugarOtraVez = scanner.nextLine();
+
+        } while (jugarOtraVez.equals("s"));
 
         System.out.println("¡Gracias por jugar!");
         scanner.close();
     }
 }
-
-
-// +---+    +---+    +---+    +---+    +---+    +---+    +---+
-// |    O   |    O   |    O   |    O   |    O   |    O   |
-// |        |    |   |   /|   |   /|\  |   /|\  |   /|\  |
-// |        |        |        |        |   /    |   / \  |
-// ===      ===      ===      ===      ===      ===      ===
